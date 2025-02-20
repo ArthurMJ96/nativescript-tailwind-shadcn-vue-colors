cc614aeae75d71925ae603ef3d9ee7816133de12
Arthur <45236453+ArthurMJ96@users.noreply.github.com>
2025-02-20 14:30:01 +0100

    add more theme files

---
+++ themes/index.d.ts
@@ -0,0 +1,15 @@
+declare enum Colors {
+  ZINC = "zinc",
+  ROSE = "rose",
+  BLUE = "blue",
+  GREEN = "green",
+  ORANGE = "orange",
+  RED = "red",
+  SLATE = "slate",
+  STONE = "stone",
+  GRAY = "gray",
+  NEUTRAL = "neutral",
+  YELLOW = "yellow",
+  VIOLET = "violet",
+}
+export = Colors;
---
+++ themes/index.js
@@ -0,0 +1,16 @@
+const Colors = {
+  ZINC: "zinc",
+  ROSE: "rose",
+  BLUE: "blue",
+  GREEN: "green",
+  ORANGE: "orange",
+  RED: "red",
+  SLATE: "slate",
+  STONE: "stone",
+  GRAY: "gray",
+  NEUTRAL: "neutral",
+  YELLOW: "yellow",
+  VIOLET: "violet",
+};
+
+module.exports = Colors;
